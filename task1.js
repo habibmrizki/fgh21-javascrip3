d10b063ebb5eabf8d2b2f32aac97930ecf2b4d56
Habib Muhammad Rizki <111440301+habibmrizki@users.noreply.github.com>
2024-06-08 21:43:01 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -14,19 +14,22 @@ const cekHariKerja = (day) => {
   });
 };
 
-cekHariKerja("Senin")
+cekHariKerja("Senin") //memanggil cekHariKerja
   .then((data) => {
+    //ketika masuk ke then masuk ke state fullfiled karena berhasil mendapatkan value dari resolve
     console.log(`Sekarang hari ${data}. Selamat bekerja!`);
-  })
+  }) //karena terjadi kesalahan tidak sesuai data
   .catch((error) => {
     console.log(error.message);
   });
 
 const cekHariKerjaAsyn = async (day) => {
   try {
-    const data = await cekHariKerja(day);
-    console.log(`Sekarang hari ${data}. Selamat bekerja!`);
+    //jalankan suatu code jika berhasil
+    const data = await cekHariKerja(day); // menunggu promise selesai dulu. Ketika selesai, hasil resolve akan di masukkan ke variable data
+    console.log(`Sekarang hari ${data}. Selamat bekerja!`); //ngeconsole untuk ${data}
   } catch (error) {
-    console.log(error.message);
+    //jika prose gagal
+    console.log(error.message); // akan memunculkan ini
   }
 };
